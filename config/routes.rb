40379bd0691d9eaed7e44801dc1daaf4a2688098
Brad van der Laan <brad.vanderlaan@gmail.com>
2016-06-07 10:10:50 -0400

    Reroute invalid URLs to the 404 page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :languages
   resources :root_strings
+  get '*path' => redirect('/404.html')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
