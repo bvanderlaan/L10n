68878fe2619829331aa476f47269b87b3b09f1ee
Brad van der Laan <brad.vanderlaan@gmail.com>
2016-06-06 21:52:11 -0400

    Add validation to the Language and TranslatedString models.

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,3 +1,5 @@
 class Language < ActiveRecord::Base
 	has_many :translated_string, dependent: :destroy
+	validates :langname, presence: true, length: { maximum: 50 }
+	validates :langcode, presence: true, length: { maximum: 10 }
 end
--- app/models/translated_string.rb
+++ app/models/translated_string.rb
@@ -1,4 +1,6 @@
 class TranslatedString < ActiveRecord::Base
   belongs_to :root_string
   belongs_to :language
+  validates :translation, presence: true
+  validates :comment, length: { maximum: 255 }
 end
