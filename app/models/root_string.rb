2a3a82a850c6c809feb9d385274750b3b2a36213
Brad van der Laan <brad.vanderlaan@gmail.com>
2016-06-07 23:12:08 -0400

    Update the Root_String model to remove any empty translation objects when being saved. This allows the user to simply remove any comment and translation from the edit root string view to delete the translations.

--- app/models/root_string.rb
+++ app/models/root_string.rb
@@ -1,6 +1,15 @@
 class RootString < ActiveRecord::Base
 	has_many :translated_strings, dependent: :destroy
-	accepts_nested_attributes_for :translated_strings, :reject_if => proc { |attributes| attributes['translation'].blank? and attributes['comment'].blank? }, :allow_destroy => true
+	accepts_nested_attributes_for :translated_strings, :reject_if => proc { |attributes| attributes[:translation].blank? and attributes[:comment].blank? and attributes[:id].blank? }, :allow_destroy => true
 	validates :string, presence: true
 	validates :comment, length: { maximum: 255 }
+	before_validation :remove_empty_translations
+		
+private
+	
+	def remove_empty_translations
+		self.translated_strings.each do |ts|
+			self.translated_strings.destroy ts if ts.translation.blank? and ts.comment.blank?
+		end		
+	end
 end
