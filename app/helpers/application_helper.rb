cfc240f1929c4a6b8bee3f510094b1ecb9ed23a8
Brad van der Laan <brad.vanderlaan@gmail.com>
2016-07-01 13:03:43 -0400

    Extract the flash message view code into a helper method. Instead of copying the view code for the flash messages into each view I've pulled it out and placed it in a helper method. I've also updated the flash message to support a close button.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def flash_messages(opts = {})
+        flash.each do |level, message|
+            concat(content_tag(:p, message, class: "alert alert-#{level} fade in") do 
+                concat content_tag(:button, '',class: "close glyphicon glyphicon-remove-circle", data: { dismiss: 'alert' })
+                concat message 
+                end)
+        end
+        nil
+    end
 end
